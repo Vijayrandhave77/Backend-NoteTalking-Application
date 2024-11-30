253a3ae95c98997110c83919605a6192e52cb833
vijay Randhave <vijayrandhave77@gmail.com>
2024-11-30 09:47:26 +0530

    first commit

--- Routes/LoginRoutes.js
+++ Routes/LoginRoutes.js
@@ -23,7 +23,7 @@ router.post('/api/login',async(req,res)=>{
         const token = generateToken(payload)
         res.cookie('jwtToken',token,{
             expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), 
-            httpOnly: false,
+            httpOnly:true,
             secure: true,  
             sameSite: 'none', 
           })
