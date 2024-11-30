7856df73c1d075ad72685a40a65b13da20d36709
vijay Randhave <vijayrandhave77@gmail.com>
2024-11-30 09:56:16 +0530

    index

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Forget  = require('./Routes/ForgetPassRoutes')
 const cors = require('cors')
 
 const corsOptions = {
-    origin: 'http://localhost:5173',  
+    origin: 'https://frontend-note-talking-application.vercel.app',  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true, 
     optionsSuccessStatus: 204  
